b37c77148cb3b2ffba4c14c4f3e937135adfa421
Anastasia Gorlina <gobi199@yandex.ru>
2023-09-14 01:01:20 +0300

    fix

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import './preloader.css'
+import './Preloader.css'
 
 const Preloader = () => {
   return (
-    <div className="preloader">
+    <div className="Preloader">
       <div className="preloader__container">
         <span className="preloader__round"></span>
       </div>
