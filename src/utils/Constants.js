3031311e014199eeb441e271d309c4825ddeaaad
Anastasia Gorlina <gobi199@yandex.ru>
2023-09-17 17:24:10 +0300

    NotFound

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://api.anastasia.gorlina.nomoreparties.co';
-//export const BASE_URL = 'http://localhost:3001';
+//export const BASE_URL = 'https://api.anastasia.gorlina.nomoreparties.co';
+export const BASE_URL = 'http://localhost:3001';
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const MOVIES_LIST_RENDER_CONFIG = {
   '1280px': { maxAmount: 12, amountToAdd: 3 },
